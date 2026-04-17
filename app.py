8a7b53ed12ba74172d860ca8010e69d213d6a5bc
Dejan Zec <dejanz878@gmail.com>
2026-04-17 23:23:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,37 @@
 import streamlit as st
+import matplotlib.pyplot as plt
 
-st.title("Player Search Dashboard")
+st.title("Player Pizza Chart")
 
 player = st.text_input("Enter player name:")
+
+def pizza_chart(values, labels, title):
+    fig, ax = plt.subplots()
+    ax.pie(values, labels=labels, autopct='%1.0f%%')
+    ax.set_title(title)
+    return fig
 
 if st.button("Show Player"):
 
     player = player.lower()
 
     if player == "messi":
-        st.success("Lionel Messi")
-        st.write("Dribbling: 95")
-        st.write("Shooting: 92")
-        st.write("Passing: 91")
+        values = [95, 92, 91]
+        labels = ["Dribbling", "Shooting", "Passing"]
+        fig = pizza_chart(values, labels, "Lionel Messi")
+        st.pyplot(fig)
 
     elif player == "ronaldo":
-        st.success("Cristiano Ronaldo")
-        st.write("Dribbling: 88")
-        st.write("Shooting: 94")
-        st.write("Physical: 93")
+        values = [88, 94, 93]
+        labels = ["Dribbling", "Shooting", "Physical"]
+        fig = pizza_chart(values, labels, "Cristiano Ronaldo")
+        st.pyplot(fig)
 
     elif player == "mbappe":
-        st.success("Kylian Mbappe")
-        st.write("Pace: 97")
-        st.write("Dribbling: 92")
-        st.write("Shooting: 89")
+        values = [97, 92, 89]
+        labels = ["Pace", "Dribbling", "Shooting"]
+        fig = pizza_chart(values, labels, "Kylian Mbappe")
+        st.pyplot(fig)
 
     else:
         st.error("Player not found")
